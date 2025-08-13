7ce88518c7427b296fca32a80e8fa27ebcb5d021
João Pedro Duarte <joao84838@gmail.com>
2025-08-13 20:31:46 -0300

    Aula 02
    Exemplo hello word Java Script e Node

---
+++ Aula02/ex1.js
@@ -0,0 +1,7 @@
+// Primeiro exemplo de código em Java Script
+
+const idade = 29; // Declaro uma constante chamada idade
+const nome = "Daniel"; // Declara uma constante chamada nome
+
+// Imprime uma mensagem no terminal
+console.log("Ola Daniel!");
