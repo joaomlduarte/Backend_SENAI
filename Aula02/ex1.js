5a713c3aa35974096b1f3a2f99080572ba35b836
João Pedro Duarte <joao84838@gmail.com>
2025-08-13 21:34:48 -0300

    Verificando tipo de variável, ex02
    Verificação de variável

--- Aula02/ex1.js
+++ Aula02/ex1.js
@@ -2,9 +2,11 @@
 
 const idade = 29; // Declaro uma constante chamada idade
 const nome = "Daniel"; // Declara uma constante chamada nome
+const inf = nome + " " + idade
 
 // Imprime uma mensagem no terminal
 console.log("Ola Daniel!");
 
 // Exibindo o valor das constantes no terminal
 console.log(`${nome} idade ${idade}`);
+console.log(inf);
---
+++ Aula02/ex2.js
@@ -0,0 +1,8 @@
+// Exemplo 02 Verificando o tipo de variável
+
+const valor = 50.5; // Constante do tipo double
+const num = 100; // constante
+const a = true ; // constante do tipo booleana
+console.log(valor, `Tipo da constante ${typeof valor}`); // typeof exibe o tipo da constante ou variável
+console.log(num, `${typeof num}`);
+console.log(a, `${typeof a}`);
