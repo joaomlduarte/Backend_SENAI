5ce4ccc6b3bd012a3dd2ac7b6f2c493229303bf7
joaomlduarte <joao84838@gmail.com>
2025-09-10 20:36:03 -0300

    aula06
    exemplos array

---
+++ aula06/ex01.js
@@ -0,0 +1,7 @@
+// Exemplo Array
+const frutas = ['Pera','Uva','Maça']; // Cria um array chamado frutas
+console.log(frutas); // exibe a lista no console
+console.log(frutas[2]); // Acessa o elemento 2 do array
+
+const frutas2 = new Array('Maça','Morango','Laranja'); // Segunda forma de criar array
+console.log(frutas2[1]); // Acessa o indice 1 do array
---
+++ aula06/ex02array.js
@@ -0,0 +1,11 @@
+// Exemplo 2 com array
+notas = [5,10,7.5];
+console.log(notas); // Exibe as notas
+
+// Remove a última nota do array
+notas.pop();
+console.log(notas);
+notas.push(9); // Adciona a nota no array
+console.log(notas);
+notas.push(10); // Adiciona notas no array
+console.log(notas);
